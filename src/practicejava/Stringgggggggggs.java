bedc4c04595a1d04dc6bec4a7dea71d1f42f1370
AyushBhardwaj <ayush7bhardwaj@gmail.com>
2025-01-29 14:23:55 +0530

    stringCommit

---
+++ src/practicejava/Stringgggggggggs.java
@@ -0,0 +1,9 @@
+package practicejava;
+
+public class Stringgggggggggs {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hi";
+        String s3 = new String("gello").intern();
+    }
+}
